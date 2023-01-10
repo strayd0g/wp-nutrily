0db20bb5d7092c0a39bc4aab9c347c9e0552ab20
strayd0g <vndrewboss@gmail.com>
2023-01-10 17:50:50 +0800

    Fix: Upload Folder

--- main.php
+++ main.php
@@ -4,7 +4,7 @@
  * Plugin Name: WP Nutrily
  * Author: TVS
  * Author URI: https://tugasvirtualsolutions.com/
- * Version: 1.0.6
+ * Version: 1.0.7
  * Description: Nutritional Plan Software.
  * Text-Domain: nutrily.de
  * 
